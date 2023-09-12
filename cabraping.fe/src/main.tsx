ffc598375a6651fdc449368608851fb66c72a5bb
Jonathan Nicolas <jonathannicolas.dev@gmail.com>
2023-09-12 08:26:55 -0700

    feat: set up react router

--- cabraping.fe/src/main.tsx
+++ cabraping.fe/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import HomeRoute from "./routes/home";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+
+    //element: <HomeRoute />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ cabraping.fe/src/routes/home.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomeRoute() {
+  return (
+    <div>
+      <Button>42 Login</Button>
+    </div>
+  );
+}
