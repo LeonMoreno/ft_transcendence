47e8705e28aba0711ff7af0314465d417fc81346
Diego-Luna <diegofcolunalopez@gmail.com>
2024-02-28 07:50:47 -0600

    backend problem solving

--- cabraping.be/entrypoint.sh
+++ cabraping.be/entrypoint.sh
@@ -1,14 +1,19 @@
+#!/bin/sh
+
+echo "----> Diego = espera"
 
 while ! nc -z database 5432; do
   sleep 0.1
 done
+
+echo "----> Diego = ejecutado"
 
 # Execute migrations
 python3 manage.py migrate users
 python3 manage.py migrate chat
 
 
-# Starts the server
+# # Starts the server
 python manage.py runserver 0.0.0.0:8000
 
 # Execute the command passed to docker run or docker-compose up
