a375d904d27001078b64765fe354a62d0956f752
Cyande319 <tristan-b@live.ca>
2024-04-26 06:37:40 -0400

    added dotenv, moved UID and SECRET to .env

--- cabraping.be/auth42/views.py
+++ cabraping.be/auth42/views.py
@@ -1,12 +1,8 @@
 # Example usage:
-UID = "u-s4t2ud-42ac1836e7066a999c4a59ecc775ff92cabdf600ea8123ee418ebc32e4a41739"
-SECRET = "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"
-TOKEN_URL = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-42ac1836e7066a999c4a59ecc775ff92cabdf600ea8123ee418ebc32e4a41739&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&response_type=code'
 
 HTTP_PROTOCOL = 'https://'
 APP42_DOMAIN = 'api.intra.42.fr'
 APP42_AUTH = '/oauth2/auth'
-APP42_OAUTH_REDIRECT = "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-42ac1836e7066a999c4a59ecc775ff92cabdf600ea8123ee418ebc32e4a41739&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&response_type=code"
 
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -14,12 +10,15 @@ from rest_framework.decorators import action
 from rest_framework import status
 from django.shortcuts import render
 from django.shortcuts import redirect
+import os
 
+UID = os.getenv('UID')
+SECRET = os.getenv('SECRET')
 
 #  Create a url will return the access autorisation_code
 def api_view(request):
     api_url =   'https://api.intra.42.fr/oauth/authorize' + \
-                '?client_id=' + "u-s4t2ud-42ac1836e7066a999c4a59ecc775ff92cabdf600ea8123ee418ebc32e4a41739" + \
+                '?client_id=' + UID + \
                 '&redirect_uri=' + "https://localhost:8080" + \
                 '&response_type=code'
 
