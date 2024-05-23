11c2780f11449a36aadc353d7005af3cfdf20154
Diego-Luna <diegofcolunalopez@gmail.com>
2024-05-23 16:07:03 -0400

    fixt it - proble wit js

--- cabraping.fe/src/pages/Game/html.js
+++ cabraping.fe/src/pages/Game/html.js
@@ -1,30 +1,30 @@
-import { getHash } from "../../utils/getHash.js";
+// import { getHash } from "../../utils/getHash.js";
 
 export function Game_html() {
-  let param = getHash();
+  // let param = getHash();
 
   return `
     <div class="container-fluid d-flex align-items-center justify-content-center mb-5 mt-5">
       <div class="row w-100">
-       
         <section class="col-12 d-flex justify-content-center">
-        <div>
-          <header class="d-flex justify-content-between">
-            <h3>
-              <span>Player A: </span>
-              <span id="left-paddle-score">0</span>
-            </h3>
-            <h3>
-              <span>Player B: </span>
-              <span id="right-paddle-score">0</span>
-            </h3>
-          </header>
+          <div>
+            <header class="d-flex justify-content-between">
+                <h3>
+                  <span>Player A: </span>
+                  <span id="left-paddle-score">0</span>
+                </h3>
+                <h3>
+                  <span>Player B: </span>
+                  <span id="right-paddle-score">0</span>
+                </h3>
 
-          <div class="bg-dark w-100 h-100 d-flex align-items-center justify-content-center p-5" >
-            <canvas id="game" class="w-100 h-100"></canvas>  <!-- Canvas se ajusta al espacio disponible después del padding -->
+              </header>
+
+              <div class="bg-dark w-100 h-100 d-flex align-items-center justify-content-center p-5" >
+                 <canvas id="game" class="w-100 h-100"></canvas>  <!-- Canvas se ajusta al espacio disponible después del padding -->
+              </div>
           </div>
         </section>
-        <div>
       </div>
     </div>
   `;
