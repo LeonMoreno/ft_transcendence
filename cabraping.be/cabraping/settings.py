a21bf98de30e596a3a15556e1fcf0fc84cc41691
Diego-Luna <diegofcolunalopez@gmail.com>
2024-06-12 21:07:10 -0400

    update .env

--- cabraping.be/cabraping/settings.py
+++ cabraping.be/cabraping/settings.py
@@ -31,7 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
